fdaf75766fc7890ceb0519c3b7e9431211e6eac6
237713070 <42698881+237713070@users.noreply.github.com>
2018-12-12 23:00:56 +0800

    201812122300
    201812122300

--- 购物车小组件/gou.js
+++ 购物车小组件/gou.js
@@ -1,11 +1,52 @@
 var value = 1;
-$('.num-box').find('.jian').click(function(){
-    // console.log('22')
-    value = value - 1;
-    $('.num-pre').val(value);
-}).end().find('.jia').click(function(){
-    // console.log('111')
-        value = value + 1;
-        $('.num-pre').val(value);
+var max = 20;
+function notIn(){
+    if(value <= 1){
+        $('.jian').addClass('min');
+    }else if(value >= max){
+        $('.jia').addClass('min');
+    }else{
+        $('.min').removeClass('min')
     }
-)
+}
+    notIn();
+
+$('.text').on('input', function() {
+    value = perseInt(this.value);
+    if(value > max){
+        value = max;
+    }else if(value >= 1 && value <= max){
+        value = parseInt(this.value);
+    }else{
+        value = 1;
+    };
+    $(this).val(value);
+    notIn();
+})
+function cont(num){
+    
+    value = parseInt($('.text').val()) + num;
+    if(value <= 1){
+        value = 1;
+    }else if(value >= max){
+        value = max;
+    }
+    $('.text').val(value);
+}
+
+// $('.num-box').find('.jian').click(function(){
+//     // console.log('22')
+//     cont(-1);
+//     notIn();
+// }).end().find('.jia').click(function(){
+//     // console.log('111')
+   
+// })
+$('.jian').add('.jia').click(function() {
+    if($(this).hasClass('jian')) {
+        cont( -1 );
+    }else {
+        cont( 1 );
+    }
+    notIn();
+})
