1ccc69099a9cc3045dbe80e22a27c3696119d34d
237713070 <42698881+237713070@users.noreply.github.com>
2019-01-14 23:42:36 +0800

    201901142342
    201901142342

--- 轮播图/JS/index.js
+++ 轮播图/JS/index.js
@@ -1,10 +1,62 @@
 var index = 0,
-    num = 6,
-    imgwidth = 590;
+    num = 5,
+    imgwidth = 590,
+    timer = undefined;
 
-$('prevBtn').click(function(){
+timerFun()
+function move(val){
+    clearTimeout(timer)
+    $('.img-box').stop( false , true );
+    if( val == 'prev'){
+        index--;
+        if(index < 0){
+            $('.img-box').css({
+                left: num * -imgwidth
+            })
+            index = 4;
+        }
+    }else if(val == 'next'){
+        index++;
+        if(index > num){
+            $('.img-box').css({
+                left: 0
+            })
+            index = 1;
+        }
+    }
+    $('.img-box').animate({
+        left: index * -imgwidth
+    },function(){
+        timerFun();
+    });
+    active($('.item').eq(index == 5 ? 0 : index));
+}
+function timerFun(){
+    timer = setTimeout(function(){
+        move('next');
+    },3000)
+}
 
-})
-$('nextBtn').click(function(){
-    
-})
+function active(dom){
+    $('.active').removeClass('active');
+    $(dom).addClass('active');
+}
+$('.prevBtn').click(function(){
+    move('prev')
+    // index--;
+    // $('.img-box').animate({
+    //     left: index * -imgwidth
+    // })
+});
+$('.nextBtn').click(function(){
+    move('next')
+    // index++;
+    // $('.img-box').animate({
+    //     left: index * -imgwidth
+    // })
+});
+$('.item').click(function(){
+    index = $(this).index();
+    // active($(this));
+    move('');
+});
